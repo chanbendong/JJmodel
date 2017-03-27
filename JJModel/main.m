dfe57fc83cb8c27033fd3e9c0bcab6f1dda99951
吴孜健 <wuzijian_dong@163.com>
2017-03-27 15:49:58 +0800

    Initial Commit

---
+++ JJModel/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JJModel
+//
+//  Created by 吴孜健 on 17/3/27.
+//  Copyright © 2017年 吴孜健. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
